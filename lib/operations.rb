112032d367e769c9a9eeb551d9fe76e9a73198ee
Chris Cheong <chrisxcheong@gmail.com>
2019-07-07 03:44:10 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,12 @@
 def unsafe?(speed)
-if speed < 40 && speed > 60 ? safe : unsafe
+if speed < 40 && speed > 60
+  retun "safe"
+else
+  return "unsafe"
 end
 
 
 
 def not_safe?(speed)
-
+speed < 40 && speed > 60 ? "safe" : "unsafe"
 end
